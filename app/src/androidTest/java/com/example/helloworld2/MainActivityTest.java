3f81aaed3a24d87e293fb9e050fa7ddfbc7e4b6c
benjenne <@Castle27>
2020-04-28 19:43:45 -0700

    Added some limited tests for the text input fields

--- app/src/androidTest/java/com/example/helloworld2/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld2/MainActivityTest.java
@@ -7,7 +7,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -21,5 +23,19 @@ public class MainActivityTest {
     public void hasTextOnScreen(){
         onView(withId(R.id.hello_world))
                 .check(matches(withText(R.string.hello_world)));
+    }
+    @Test
+    public void hasFooter(){
+        onView(withId(R.id.author))
+                .check(matches(withText(R.string.author)));
+    }
+    @Test
+    public void testTextInput(){
+        onView(withId(R.id.input_name))
+                .perform(typeText("Ben")).check(matches(withText("Ben")));
+        onView(withId(R.id.input_email))
+                .perform(typeText("Ben@gmail.com")).check(matches(withText("Ben@gmail.com")));
+        onView(withId(R.id.input_username))
+                .perform(typeText("Ben10")).check(matches(withText("Ben10")));
     }
 }
