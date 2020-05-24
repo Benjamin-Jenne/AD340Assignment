f9486647af75b08dd462236ff52ab58e2d41469f
benjenne <@Castle27>
2020-05-23 18:00:51 -0700

    Added some low quality tests to the recycler views in matches.

--- app/src/androidTest/java/com/example/helloworld2/SubmitActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld2/SubmitActivityTest.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import androidx.test.rule.ActivityTestRule;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 
 public class SubmitActivityTest {
     @Rule
@@ -59,10 +66,19 @@ public class SubmitActivityTest {
         onView(withId(R.id.description))
                 .check(matches(withText(Constants.TEST_DESCRIPTION)));
     }
+    //Referenced https://spin.atomicobject.com/2016/04/15/espresso-testing-recyclerviews/
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
     @Test
+    //Referenced https://stackoverflow.com/questions/27396583/how-to-click-on-an-item-inside-a-recyclerview-in-espresso
     public void testMatchesTab(){
         onView(withText("MATCHES")).perform(click());
-        onView(withId(R.id.matches_placeholder)).check(matches(withText(R.string.matches)));
+        onView(withId(R.id.my_recycler_view)).perform(actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.my_recycler_view)).perform(actionOnItemAtPosition(2, click()));
+        onView(withId(R.id.my_recycler_view)).perform(actionOnItemAtPosition(3, click()));
+        onView(withId(R.id.my_recycler_view)).perform(actionOnItemAtPosition(4, click()));
+        onView(withId(R.id.my_recycler_view)).perform(actionOnItemAtPosition(5, click()));
     }
     @Test
     public void testSettingsTab(){
