3a5fb47a354bc3cea1c8b547bdaba8ab7ea1ff79
benjenne <@Castle27>
2020-06-02 17:45:11 -0700

    Added some frontend stuff

--- app/src/main/java/com/example/helloworld2/SettingsFragment.java
+++ app/src/main/java/com/example/helloworld2/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,7 +12,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
-    private TextView settingsPlaceholder;
+    private TextView desiredMinAgeText;
+    private TextView desiredMaxAgeText;
+    private TextView desiredRangeText;
+    private SeekBar desiredMinAgeBar;
+    private SeekBar desiredMaxAgeBar;
+    private SeekBar desiredRange;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -20,7 +26,14 @@ public class SettingsFragment extends Fragment {
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        settingsPlaceholder = getActivity().findViewById(R.id.settings_placeholder);
-        settingsPlaceholder.setText(R.string.settings);
+        desiredMinAgeText = getActivity().findViewById(R.id.desired_Min_Age_Text);
+        desiredMaxAgeText = getActivity().findViewById(R.id.desired_Max_Age_Text);
+        desiredRangeText = getActivity().findViewById(R.id.desired_Range_Text);
+        desiredMinAgeText.setText(R.string.desired_min_age);
+        desiredMaxAgeText.setText(R.string.desired_max_age);
+        desiredRangeText.setText(R.string.desired_range);
+        desiredMinAgeBar = getActivity().findViewById(R.id.desired_Min_Age_Bar);
+        desiredMaxAgeBar = getActivity().findViewById(R.id.desired_Max_Age_Bar);
+        desiredRange = getActivity().findViewById(R.id.desired_Range_Bar);
     }
 }
